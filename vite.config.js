f92e3a7dffbf6818c0e5963728261c842480c1c0
apple97 <rkt1313@naver.com>
2025-10-22 10:52:28 +0900

    페이지스 추가

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tany_front/',
   server: {
     port: 3000,
     open: true
